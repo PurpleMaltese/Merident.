fca802e4ec902ff4e962634af4110c184775dac4
Oliver Ricalde <oliverricaldec@gmail.com>
2025-09-16 23:38:11 -0500

    inicio

---
+++ src/main/java/com/mycompany/merident/Merident.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.merident;
+
+/**
+ *
+ * @author olive
+ */
+public class Merident {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
